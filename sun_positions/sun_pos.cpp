5b769b5b1b82c9f0c218a7fed24f6cad66e4f948
Rafael Nardi <nardi.rafael@gmail.com>
2022-04-25 10:44:36 -0300

    refatorando sun_pos.cpp

--- sun_positions/sun_pos.cpp
+++ sun_positions/sun_pos.cpp
@@ -1,3 +1,4 @@
+#include <ostream>
 #include <stdio.h>
 #include <iostream>
 #include <cmath>
@@ -25,9 +26,9 @@ float ang_hor_rad(float hora_local){
 	return deg_to_rad(ang_hor_deg);
 }
 
-float decl_calculation(int NDA){
+float decl_calculation(int NDA){ //a declinação mede o ângulo da linha Terra-Sol com o plano do equador
 
-	float angulo_rad = deg_to_rad(360*(NDA - 80)/365.25);
+	float angulo_rad = deg_to_rad(360*(NDA - 80)/365.25); //dia 80 é o 21 de março: equinócio de outono
 	float decl = 23.45*sin(angulo_rad); //em graus
 	return deg_to_rad(decl);
 }
@@ -43,29 +44,41 @@ class vetor_3d {
 	}
 	
 	float scalar_prod(vetor_3d vetor_2){
-		return sqrt(this->coord[0] * vetor_2.coord[0] + this->coord[1] * vetor_2.coord[1] + this->coord[2] * vetor_2.coord[2]);
+		float square = 0.0;
+		for (short i=0; i<3; i++) {
+			square += this->coord[i] * vetor_2.coord[i];
+		}
+		return sqrt(square);
 	}
-
 };
 
-int main()
-{
-	int NDA = NDA_calculation(30,5);
+float * sin_Alt_sin_Azim_calculation(int NDA, float lat, float local_time){
 
-	float rad_hor = ang_hor_rad(17.4);
+	float hora_local = local_time;
+	float hor_rad = ang_hor_rad(hora_local);
 
-	float lat = -18.9051; //Jaguaré em graus
-	/* float lat = -14.7973; //Ilhéus em graus */
-	float rad_lat = deg_to_rad(lat);
+	float lat_rad = deg_to_rad(lat);
 
-	float rad_decl = decl_calculation(NDA);
+	float decl_rad = decl_calculation(NDA);
 
-	float sin_Alt = sin(rad_lat)*sin(rad_decl) + cos(rad_lat)*cos(rad_decl)*cos(rad_hor);
+	float sin_Alt = sin(lat_rad)*sin(decl_rad) + cos(lat_rad)*cos(decl_rad)*cos(hor_rad); //#############
 	float cos_Alt = sqrt(1 - pow(sin_Alt,2));
 	//ângulo/distância zenital é o complementar da altura Alt
 	float zen = acos(sin_Alt);
 
-	float sin_Azim = cos(rad_decl)*sin(rad_hor)/cos_Alt;
+	float sin_Azim = cos(decl_rad)*sin(hor_rad)/cos_Alt;  //#############
+	float cos_Azim = sqrt(1 - pow(sin_Azim,2));
+
+	static float result[2] = {sin_Alt, sin_Azim};
+
+	return result;
+}
+
+vetor_3d sun_pos_in_cartesian_coord(float * sin_Alt_sin_Azim_Array){
+
+	float sin_Alt = sin_Alt_sin_Azim_Array[0];
+	float sin_Azim = sin_Alt_sin_Azim_Array[1];
+	float cos_Alt = sqrt(1 - pow(sin_Alt,2));
 	float cos_Azim = sqrt(1 - pow(sin_Azim,2));
 
 	//posição do sol em coordenadas cartezianas:
@@ -73,19 +86,49 @@ int main()
 	float s_x = cos_Alt*sin_Azim; //projeção leste-oeste (positivo se ao leste)
 	float s_y = cos_Alt*cos_Azim; //projeção norte-sul (positivo se ao norte)
 
-	//teste de cálculo da norma do vetor de localização do sol:
-	vetor_3d s(s_x, s_y, s_z);
-	vetor_3d r(s_x, s_y, s_z);
+	static vetor_3d s(s_x, s_y, s_z);
+
+	return s;
+}
+
+void log_entrada(int NDA, float latitude, float local_time){
+	
+	std::cout << "Latitude: " << latitude << std::endl;
+	std::cout << "Hora local: " << local_time << std::endl;
+	std::cout << "NDA: "<< NDA << std::endl;
+}
+
+void log_angulos(float * sin_Alt_sin_Azim_Array ){
+	std::cout << "Valor do Azimute: "<< asin(sin_Alt_sin_Azim_Array[1]) << std::endl;
+	std::cout << "Valor do ângulo Zenital: "<< acos(sin_Alt_sin_Azim_Array[0]) << std::endl;
+}
+
+void log_sun_position(vetor_3d s){
+	std::cout << "Valor da Projeção vertical: "<< s.coord[2] << std::endl;
+	std::cout << "Valor da Projeção leste-oeste: "<< s.coord[0] << std::endl;
+	std::cout << "Valor da Projeção norte-sul: "<< s.coord[1] << std::endl;
+}
+
+int main()
+{
+	int NDA = NDA_calculation(24,3);
+	float hora_local = 17.693;
+	float lat = -18.9051; //Jaguaré em graus
+	/* float lat = -14.7973; //Ilhéus em graus */
+
+	float * sin_Alt_sin_Azim_Array = sin_Alt_sin_Azim_calculation(NDA, lat, hora_local);
+
+	vetor_3d s = sun_pos_in_cartesian_coord(sin_Alt_sin_Azim_Array);
+
+	vetor_3d r(s.coord[0], s.coord[1], s.coord[2]);
+
 	float norma = s.scalar_prod(r);
 
-	std::cout << "NDA: "<< NDA << std::endl;
-	std::cout << "Valor do Azimute: "<< asin(sin_Azim) << std::endl;
-	std::cout << "Valor do ângulo Zenital: "<< zen << std::endl;
-	std::cout << "Valor da Projeção vertical: "<< s_z << std::endl;
-	std::cout << "Valor da Projeção leste-oeste: "<< s_x << std::endl;
-	std::cout << "Valor da Projeção norte-sul: "<< s_y << std::endl;
-	std::cout << "Testando cálculo da Norma: "<< norma << std::endl;
+	log_entrada(NDA, lat, hora_local);
+	log_angulos(sin_Alt_sin_Azim_Array);
+	log_sun_position(s);
 
+	std::cout << "Testando cálculo da Norma: "<< norma << std::endl;
 
 	return 0;
 }
