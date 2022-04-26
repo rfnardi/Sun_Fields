1c3c453568c52eefb5df4d61436a488964dc96f8
Rafael Nardi <nardi.rafael@gmail.com>
2022-04-26 16:19:30 -0300

    separação das funções do arquivo do main(). Preparando pra refatorar.

--- sun_positions/all_functions.cpp
+++ sun_positions/all_functions.cpp
@@ -1,38 +1,7 @@
-#include <ostream>
-#include <stdio.h>
 #include <iostream>
 #include <cmath>
-#define M_PI 3.14159265358979323846
 #define J_0 1367
-
-int NDA_calculation(int month_day, int month){
-
-	int month_size[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	int NDA = 0;
-	for (short i = 0; i < month; ++i) {
-		NDA += month_size[i];
-	}
-	NDA += month_day;
-
-	return NDA;
-}
-
-float deg_to_rad(float deg){ //transforma graus em radianos
-	return 2*M_PI*deg/360;
-}
-
-float ang_hor_rad(float hora_local){
-	
-	float ang_hor_deg = (12 - hora_local)*15; //em graus 
-	return deg_to_rad(ang_hor_deg);
-}
-
-float decl_calculation(int NDA){ //a declinação mede o ângulo da linha Terra-Sol com o plano do equador
-
-	float angulo_rad = deg_to_rad(360*(NDA - 80)/365.25); //dia 80 é o 21 de março: equinócio de outono
-	float decl = 23.45*sin(angulo_rad); //em graus
-	return deg_to_rad(decl);
-}
+#define M_PI 3.14159265358979323846
 
 class vetor_3d {
 	public:
@@ -59,48 +28,17 @@ class vetor_3d {
 	}
 };
 
-float * sin_Alt_sin_Azim_calculation(int NDA, float lat, float local_time, float * result){
 
-	float hora_local = local_time;
-	float hor_rad = ang_hor_rad(hora_local);
+int NDA_calculation(int month_day, int month){
 
-	float lat_rad = deg_to_rad(lat);
+	int month_size[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int NDA = 0;
+	for (short i = 0; i < month; ++i) {
+		NDA += month_size[i];
+	}
+	NDA += month_day;
 
-	float decl_rad = decl_calculation(NDA);
-
-	float sin_Alt = sin(lat_rad)*sin(decl_rad) + cos(lat_rad)*cos(decl_rad)*cos(hor_rad); //THIS GUY
-	float cos_Alt = sqrt(1 - pow(sin_Alt,2));
-
-	//ângulo/distância zenital é o complementar da altura Alt
-	/* float zen = acos(sin_Alt); */
-
-	float sin_Azim = cos(decl_rad)*sin(hor_rad)/cos_Alt;  //AND THIS GUY
-	/* float cos_Azim = sqrt(1 - pow(sin_Azim,2)); */
-
-	float array[2] = {sin_Alt, sin_Azim};
-
-	result = array;
-
-	return result;
-}
-
-vetor_3d sun_pos_in_cartesian_coord(float * sin_Alt_sin_Azim_Array, vetor_3d result){
-
-	float sin_Alt = sin_Alt_sin_Azim_Array[0];
-	float sin_Azim = sin_Alt_sin_Azim_Array[1];
-	float cos_Alt = sqrt(1 - pow(sin_Alt,2));
-	float cos_Azim = sqrt(1 - pow(sin_Azim,2));
-
-	//posição do sol em coordenadas cartezianas:
-	float s_z = sin_Alt;
-	float s_x = cos_Alt*sin_Azim; //projeção leste-oeste (positivo se ao leste)
-	float s_y = cos_Alt*cos_Azim; //projeção norte-sul (positivo se ao norte)
-
-	vetor_3d s(s_x, s_y, s_z);
-
-	result = s;
-
-	return result;
+	return NDA;
 }
 
 void log_entrada(int NDA, float latitude, float local_time){
@@ -151,6 +89,69 @@ vetor_3d get_unitary_vector(vetor_3d v, vetor_3d result){
 	return result;
 }
 
+float deg_to_rad(float deg){ //transforma graus em radianos
+	return 2*M_PI*deg/360;
+}
+
+float ang_hor_rad(float hora_local){
+	
+	float ang_hor_deg = (12 - hora_local)*15; //em graus 
+	return deg_to_rad(ang_hor_deg);
+}
+
+float decl_calculation(int NDA){ //a declinação mede o ângulo da linha Terra-Sol com o plano do equador
+
+	float angulo_rad = deg_to_rad(360*(NDA - 80)/365.25); //dia 80 é o 21 de março: equinócio de outono
+	float decl = 23.45*sin(angulo_rad); //em graus
+	return deg_to_rad(decl);
+}
+
+
+float * sin_Alt_sin_Azim_calculation(int NDA, float lat, float local_time, float * result){
+
+	float hora_local = local_time;
+	float hor_rad = ang_hor_rad(hora_local);
+
+	float lat_rad = deg_to_rad(lat);
+
+	float decl_rad = decl_calculation(NDA);
+
+	float sin_Alt = sin(lat_rad)*sin(decl_rad) + cos(lat_rad)*cos(decl_rad)*cos(hor_rad); //THIS GUY
+	float cos_Alt = sqrt(1 - pow(sin_Alt,2));
+
+	//ângulo/distância zenital é o complementar da altura Alt
+	/* float zen = acos(sin_Alt); */
+
+	float sin_Azim = cos(decl_rad)*sin(hor_rad)/cos_Alt;  //AND THIS GUY
+	/* float cos_Azim = sqrt(1 - pow(sin_Azim,2)); */
+
+	float array[2] = {sin_Alt, sin_Azim};
+
+	result = array;
+
+	return result;
+}
+
+vetor_3d sun_pos_in_cartesian_coord(float * sin_Alt_sin_Azim_Array, vetor_3d result){
+
+	float sin_Alt = sin_Alt_sin_Azim_Array[0];
+	float sin_Azim = sin_Alt_sin_Azim_Array[1];
+	float cos_Alt = sqrt(1 - pow(sin_Alt,2));
+	float cos_Azim = sqrt(1 - pow(sin_Azim,2));
+
+	//posição do sol em coordenadas cartezianas:
+	float s_z = sin_Alt;
+	float s_x = cos_Alt*sin_Azim; //projeção leste-oeste (positivo se ao leste)
+	float s_y = cos_Alt*cos_Azim; //projeção norte-sul (positivo se ao norte)
+
+	vetor_3d s(s_x, s_y, s_z);
+
+	result = s;
+
+	return result;
+}
+
+
 //calcula a correção na constante solar devido à variação na distância Terra-Sol ~ (d/D)^2
 float J_elliptic_correction(int NDA){ 
 	float J = J_0*(1 + 0.033*cos(deg_to_rad(360*NDA/365.25)));
@@ -194,32 +195,3 @@ float one_mirror_power(vetor_3d s, vetor_3d R, int NDA){
 	return power;
 }
 
-int main()
-{
-	int NDA = NDA_calculation(25,3);
-	float lat = -18.9051; //Jaguaré em graus
-	/* float lat = -14.7973; //Ilhéus em graus */
-
-	vetor_3d R(0,-10,0); //espelho localizado a 12 metros ao sul do ponto focal
-
-	float * sin_Alt_sin_Azim_Array;
-	float power;
-	float hora_local = 6.0;
-	vetor_3d s(0,0,0);
-	while(hora_local<18){
-		sin_Alt_sin_Azim_Array = sin_Alt_sin_Azim_calculation(NDA, lat, hora_local, sin_Alt_sin_Azim_Array);
-		s = sun_pos_in_cartesian_coord(sin_Alt_sin_Azim_Array, s);
-		power = one_mirror_power(s, R, NDA);
-		std::cout << "Hora local: "<< hora_local ;
-		std::cout << "\t ; Projeção vertical do sol: " << s.coord[2] ; 
-		std::cout << "\t ; Potência: "<< power << std::endl;
-		hora_local += 1;
-	}
-
-	/* log_entrada(NDA, lat, hora_local); */
-	/* log_angulos(sin_Alt_sin_Azim_Array); */
-	/* log_sun_position(s); */
-
-
-	return 0;
-}
