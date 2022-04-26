d60730e0bb82b72a944935a1768026de13c0dbd1
Rafael Nardi <nardi.rafael@gmail.com>
2022-04-25 22:26:40 -0300

    cálculo da potência refletida por um espelho escrito. Erro acontecendo no cálculo do vetor normal ao espelho: nan

--- sun_positions/sun_pos.cpp
+++ sun_positions/sun_pos.cpp
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <iostream>
 #include <cmath>
-# define M_PI 3.14159265358979323846
+#define M_PI 3.14159265358979323846
+#define J_0 1367
 
 int NDA_calculation(int month_day, int month){
 
@@ -50,6 +51,12 @@ class vetor_3d {
 		}
 		return sqrt(square);
 	}
+
+	void log_coords(){
+		std::cout << "Coordenada x: "<< this->coord[0] << std::endl;
+		std::cout << "Coordenada y: "<< this->coord[1] << std::endl;
+		std::cout << "Coordenada z: "<< this->coord[2] << std::endl;
+	}
 };
 
 float * sin_Alt_sin_Azim_calculation(int NDA, float lat, float local_time){
@@ -61,13 +68,14 @@ float * sin_Alt_sin_Azim_calculation(int NDA, float lat, float local_time){
 
 	float decl_rad = decl_calculation(NDA);
 
-	float sin_Alt = sin(lat_rad)*sin(decl_rad) + cos(lat_rad)*cos(decl_rad)*cos(hor_rad); //#############
+	float sin_Alt = sin(lat_rad)*sin(decl_rad) + cos(lat_rad)*cos(decl_rad)*cos(hor_rad); //THIS GUY
 	float cos_Alt = sqrt(1 - pow(sin_Alt,2));
-	//ângulo/distância zenital é o complementar da altura Alt
-	float zen = acos(sin_Alt);
 
-	float sin_Azim = cos(decl_rad)*sin(hor_rad)/cos_Alt;  //#############
-	float cos_Azim = sqrt(1 - pow(sin_Azim,2));
+	//ângulo/distância zenital é o complementar da altura Alt
+	/* float zen = acos(sin_Alt); */
+
+	float sin_Azim = cos(decl_rad)*sin(hor_rad)/cos_Alt;  //AND THIS GUY
+	/* float cos_Azim = sqrt(1 - pow(sin_Azim,2)); */
 
 	static float result[2] = {sin_Alt, sin_Azim};
 
@@ -105,8 +113,69 @@ void log_angulos(float * sin_Alt_sin_Azim_Array ){
 
 void log_sun_position(vetor_3d s){
 	std::cout << "Valor da Projeção vertical: "<< s.coord[2] << std::endl;
-	std::cout << "Valor da Projeção leste-oeste: "<< s.coord[0] << std::endl;
-	std::cout << "Valor da Projeção norte-sul: "<< s.coord[1] << std::endl;
+	std::cout << "Valor da Projeção leste-oeste: "<< s.coord[0] << std::endl; //positivo ao leste
+	std::cout << "Valor da Projeção norte-sul: "<< s.coord[1] << std::endl; //positivo ao norte
+}
+
+vetor_3d get_normal_vector(vetor_3d s, vetor_3d r){
+
+	float denominador = sqrt(2)*sqrt(1 - s.scalar_prod(r));
+	float n[3];
+	for (short i = 0; i < 3; ++i) {
+		n[i] =(s.coord[i] - r.coord[i])/denominador; 
+	}
+
+	static vetor_3d normal_vector(n[0],n[1],n[2]);
+
+	return normal_vector;
+}
+
+vetor_3d get_unitary_vector(vetor_3d v){
+	float v_hat[3];
+	for (short i = 0; i < 3; ++i) {
+		v_hat[i] = v.coord[i]/v.scalar_prod(v); 
+	}
+	static vetor_3d unitary(v_hat[0],v_hat[1],v_hat[2]);
+
+	return unitary;
+}
+
+//calcula a correção na constante solar devido à variação na distância Terra-Sol ~ (d/D)^2
+float J_elliptic_correction(int NDA){ 
+	float J = J_0*(1 + 0.033*cos(deg_to_rad(360*NDA/365.25)));
+	return J;
+}
+
+//distância de atmosfera cortada pelo raio de luz em termos do ângulo zenital
+float atm_cross_distance(float zenital_angle){ 
+	float A = 12000; //12 km
+	float R_t = 12700000; //12.700 km
+
+	return (pow(A,2) + 2*A*R_t)/(2*R_t*cos(zenital_angle));
+}
+
+float refl_power_from_scalar_product(vetor_3d n, vetor_3d s, float J){
+	float power = 0.47*J*n.scalar_prod(s);
+
+	return power;
+}
+
+float one_mirror_power(vetor_3d s, vetor_3d R, int NDA){
+	vetor_3d r = get_unitary_vector(R);
+	std::cout << "Coordenadas de r unitário:" << std::endl;
+	r.log_coords();
+
+	vetor_3d n = get_normal_vector(s, r);
+	std::cout << "Coordenadas do vetor normal à superfície n unitário:" << std::endl;
+	n.log_coords();
+
+	float J = J_elliptic_correction(NDA);
+	std::cout << "Valor de J: "<< J << std::endl;
+
+	float power = refl_power_from_scalar_product(n, s, J);
+	std::cout << "Valor de power: "<< power << std::endl;
+
+	return power;
 }
 
 int main()
@@ -119,16 +188,15 @@ int main()
 	float * sin_Alt_sin_Azim_Array = sin_Alt_sin_Azim_calculation(NDA, lat, hora_local);
 
 	vetor_3d s = sun_pos_in_cartesian_coord(sin_Alt_sin_Azim_Array);
+	vetor_3d R(0,-12,0); //espelho localizado a 12 metros ao sul do ponto focal
 
-	vetor_3d r(s.coord[0], s.coord[1], s.coord[2]);
-
-	float norma = s.scalar_prod(r);
+	float power = one_mirror_power(s, R, NDA);
 
 	log_entrada(NDA, lat, hora_local);
 	log_angulos(sin_Alt_sin_Azim_Array);
 	log_sun_position(s);
+	std::cout << "Potência de um espelho: "<< power << std::endl;
 
-	std::cout << "Testando cálculo da Norma: "<< norma << std::endl;
 
 	return 0;
 }
