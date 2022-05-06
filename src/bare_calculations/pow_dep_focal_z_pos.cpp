da3469e708896c383f07b30a0f7f000130086fb9
Rafael Nardi <nardi.rafael@gmail.com>
2022-05-06 09:01:11 -0300

    criando novas pastas para acomodar contas com as correções de atenuação da radiação que virão na sequência

--- src/bare_calculations/pow_dep_focal_z_pos.cpp
+++ src/bare_calculations/pow_dep_focal_z_pos.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stdio.h>
-#include "./all_functions.cpp"
+#include "../all_functions.cpp"
 #include <vector>
 
 int main()
@@ -12,17 +12,21 @@ int main()
 	float power = 0.0;
 	float hora_local = 12.0;
 	vetor_3d mirror_place(0,0,0);
-	float r = 10.0;
-	float theta = M_PI;
+	float south_distance = 10.0;
+	float z_pos = 0.0;
 	vetor_3d s(0,0,0);
 	s = get_sun_position(NDA, lat, hora_local,s);
-	std::cout << "theta" << ";" << "power" << std::endl;
-	while (theta < 2*M_PI) {
-		mirror_place.transf_coord_from_spher_to_cart(r,theta,M_PI/2);
+	std::cout << "z_pos" << ";" << "power" << std::endl;
+	while (z_pos > -20.1) {
+		mirror_place.reset_coord(0.0,-south_distance,z_pos);
 		power = one_mirror_power(s, mirror_place, NDA);
-		std::cout << theta << ";" << power << std::endl;
-		theta += 0.1;
+		/* std::cout << -z_pos << ";" << power << std::endl; */
+		z_pos -= 0.1;
 	}
+
+	float zen = acos(sin_Alt_calculation(NDA, lat, hora_local));
+
+	std::cout << "ângulo zenital: " << zen << std::endl;
 
 
 	return 0;
