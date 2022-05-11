a28a73b69b6209593e79fe06a02331bf3ce7ee77
Rafael Nardi <nardi.rafael@gmail.com>
2022-05-11 15:32:41 -0300

    correção na expressão de determinação do preciptable water usando a fórmula do paper original do Leckner (1978) ao invés da expressão do Iqbal. Primeiros testes mostram dados mais próximos do esperado. Falta certificar.

--- src/atenuation_corrections/pow_one_mirror_intra_day_correction.cpp
+++ src/atenuation_corrections/pow_one_mirror_intra_day_correction.cpp
@@ -5,8 +5,8 @@
 
 int main()
 {
-	int NDA = NDA_calculation(25,3);
-	float lat = -18.9051; //Jaguaré em graus
+	int NDA = NDA_calculation(10,4);
+	float lat = -18.9051; 
 	/* float lat = -14.7973; //Ilhéus em graus */
 
 	vetor_3d R(0,-10,0); 
@@ -14,17 +14,17 @@ int main()
 	float sin_Alt, sin_Azim;
 	float power = 0.0;
 	float hora_local;
-	float J;
+	float J = 0.0;
 
-	float rel_air_humid = 0.70; 
+	float rel_air_humid = 0.001; 
 	float altitude = 70; 
-	float d = 200; 
+	float d = 100; 
 	float temp_Kelvin = 393.0;
 	float mirror_area = 1.0;
 
 	/* log_entrada(NDA, lat, hora_local); */
 
-	std::cout << "HORA_LOCAL" << ";" << "POWER" << ";" << "ZEN" << ";" << "AZIM" << ";" << "S_z" << std::endl;
+	/* std::cout << "HORA_LOCAL" << ";" << "POWER" << ";" << "ZEN" << ";" << "AZIM" << ";" << "S_z" << std::endl; */
 	vetor_3d s(0,0,0);
 	for (hora_local = 6.0; hora_local < 18; hora_local += 0.08){
 
@@ -33,15 +33,18 @@ int main()
 		s = sun_pos_in_cartesian_coord(sin_Alt, sin_Azim, s);
 		J = corrected_irradiance(NDA, lat, hora_local,  rel_air_humid, altitude, d, temp_Kelvin);
 
-		power = one_mirror_corrected_power(s, R, J, mirror_area);
+		std::cout <<"Hora local: " << hora_local << " ; S_z: " << s.coord[2] << " ; J: " << J << std::endl;
 
-		if (sin_Alt > 0.0){
-			std::cout << hora_local << ";" 
-			<<	power << ";" 
-			<< acos(sin_Alt) << ";" 
-			<< asin(sin_Azim) << ";" 
-			<< sin_Alt << std::endl;
-		}
+		/* power = one_mirror_corrected_power(s, R, J, mirror_area); */
+
+		/* if (sin_Alt > 0.0){ */
+		/* 	std::cout << hora_local << ";" */ 
+		/* 	<<	power << ";" */ 
+		/* 	<< acos(sin_Alt) << ";" */ 
+		/* 	<< asin(sin_Azim) << ";" */ 
+		/* 	<< sin_Alt << std::endl; */
+		/* } */
+
 	}
 
 	return 0;
