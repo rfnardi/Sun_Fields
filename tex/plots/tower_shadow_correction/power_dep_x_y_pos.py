e53ae38bb652cbf20f795986ffdec3a2feb7c96c
Rafael Nardi <nardi.rafael@gmail.com>
2023-01-26 18:09:08 -0300

    apresentação quase terminada. Falta slides de encaminhamentos futuros

--- tex/plots/tower_shadow_correction/power_dep_x_y_pos.py
+++ tex/plots/tower_shadow_correction/power_dep_x_y_pos.py
@@ -6,53 +6,78 @@ import matplotlib.pyplot as plt
 # Este código mostra como a contribuição de cada heliostato 
 # varia ao longo de uma região quadrada às 8:00 e ao meio-dia e a média ao longo do dia.
 
-colunas_desejadas = ['x','y','power']
+def set_plot(file_choice, data_filename_ending):
+    datapath = '../../../data/tower_shadow_correction/'
+    data_file = datapath + 'average_tower_' + data_filename_ending[file_choice] + '.dat'
+    colunas_desejadas = ['x','y','power']
+    full_data = pd.read_csv(data_file, usecols=colunas_desejadas)
+    max_power = full_data['power'].max()
 
-# full_data = pd.read_csv('../../../data/tower_shadow_correction/square_grid_along_day.dat',usecols=colunas_desejadas)
-# full_data = pd.read_csv('../../../data/tower_shadow_correction/dados.dat',usecols=colunas_desejadas)
+    # full_data = pd.read_csv('../../../data/tower_shadow_correction/square_grid_8am.dat',usecols=colunas_desejadas)
+    # full_data = pd.read_csv('../../../data/tower_shadow_correction/square_grid_12am.dat',usecols=colunas_desejadas)
 
-# full_data = pd.read_csv('../../../data/tower_shadow_correction/square_grid_8am.dat',usecols=colunas_desejadas)
+    which_data = int(input('Selecionar heliostatos 5% mais potentes? 1=sim, 0=não '))
+    if which_data:
+        dados = full_data[ full_data['power'] > 0.95*max_power]
+        title_top_power = 'Potência média diária - top 5%. Altura da torre: ' + data_filename_ending[file_choice]
+    else:
+        dados = full_data
+        title_top_power = 'Potência média diária em todo o campo. Altura da torre: ' + data_filename_ending[file_choice]
 
-full_data = pd.read_csv('../../../data/tower_shadow_correction/square_grid_12am.dat',usecols=colunas_desejadas)
+    # dados = full_data[ full_data['power'] < 100]
+    min_power = dados['power'].min()
 
-max_power = full_data['power'].max()
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
 
-# dados = full_data[ full_data['power'] > 0.95*max_power]
-# dados = full_data[ full_data['power'] < 100]
-dados = full_data
-min_power = dados['power'].min()
+    plt.title(title_top_power)
+    # plt.title('Potência refletida às 8:00')
+    # plt.title('Potência refletida às 12:00')
 
-x_data = dados['x']
-y_data = dados['y']
-z_data = dados['power']
+    ax.set_xlabel('x-leste-oeste')
+    ax.set_ylabel('y-sul-norte')
+    ax.set_zlabel('Potência (W)')
 
-#figura e um eixo 3D
-fig = plt.figure()
-ax = plt.axes(projection='3d')
+    ax.plot_trisurf(dados['x'], dados['y'], dados['power'], cmap='jet')
+    ax.view_init(azim=61, elev=12)
 
-plt.title('Potência média refletida ao longo do dia')
-# plt.title('Potência refletida às 8:00')
-# plt.title('Potência refletida às 12:00')
-
-#rótulos dos eixos
-ax.set_xlabel('x-leste-oeste')
-ax.set_ylabel('y-sul-norte')
-ax.set_zlabel('Potência (W)')
+    return max_power, min_power, dados['x'].count(), dados['power'].sum(), which_data
 
 
-ax.plot_trisurf(x_data, y_data, z_data, cmap='jet')
-ax.view_init(azim=61, elev=12)
+#-----------------------------------------
 
-# plt.savefig('square_grid_along_day', dpi=300)
-# plt.savefig('square_grid_12am', dpi=300)
-# plt.savefig('square_grid_8am', dpi=300)
 
-# print("Max: ", max_power)
-# print("Min: ", min_power)
-# print("Quantidade de pontos selecionados: ", dados['x'].count())
-# # print("Quantidade total de pontos: ", full_data['x'].count())
-# print("Potência total recolhida nos heliostatos selecionados: ", dados['power'].sum())
 
-# plt.rcParams['figure.figsize'] = (15,15)
-plt.show()
+data_filename_ending = ['20m','40m','60m','70m','80m']
+select_one_single_file = int(input('Exibição interativa de um gráfico (1) ou geração de arquivos (0)? '))
+
+
+if select_one_single_file:
+    print('Altura da torre , Opção')
+    for i in range(0,5):
+        print(data_filename_ending[i], ' , ', i)
+    file_choice = int(input('Altura da torre: '))
+
+    Max, Min, Qtd_Heliost, Total_Power, which_data = set_plot(file_choice, data_filename_ending)
+
+    print("Max: ", Max)
+    print("Min: ", Min)
+    print("Quantidade de heliostatos selecionados: ", Qtd_Heliost)
+    print("Potência total recolhida nos heliostatos selecionados: ", Total_Power)
+
+    plt.show()
+else:
+    for i in range(0,5):
+        print('Trabalhando com dados para torre de ', data_filename_ending[i])
+        do_work = int(input('Proceder com criação do gráfico referente a este arquivo? 1=sim, 0=não: '))
+        if do_work:
+            Max, Min, Qtd_Heliost, Total_Power, which_data = set_plot(i, data_filename_ending)
+            if which_data:
+                plt.savefig('square_grid_along_day_'+data_filename_ending[i]+'top_5_most_powerfull', dpi=300)
+            else: 
+                plt.savefig('square_grid_along_day_'+data_filename_ending[i]+'full_grid', dpi=300)
+
+    # plt.savefig('square_grid_12am', dpi=300)
+    # plt.savefig('square_grid_8am', dpi=300)
+
 
