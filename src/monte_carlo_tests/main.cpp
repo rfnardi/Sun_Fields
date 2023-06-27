e31c574b27acb74fe6f54ec29e81473f56690d86
Rafael Nardi <nardi.rafael@gmail.com>
2023-06-27 09:59:31 -0300

    movendo teste da função pick_point_inside_mirror_region para a pasta recém criada: monte_carlo_tests

--- src/monte_carlo_tests/main.cpp
+++ src/monte_carlo_tests/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "./heliostato.h"
 #include "./bare_functions.h"
-/* #include "heliostato.cpp" */
+/* #include "./heliostato.cpp" */
 
 int main(){
 
@@ -12,6 +12,9 @@ vetor_3d result;
 h1.set_point_inside_mirror_region(0.2,0.5);
 
 result = h1.pick_point_inside_mirror_region(0.1,0.3, result);
+std::cout << "Coordenada x: " << result.coord[0] << std::endl; 
+std::cout << "Coordenada y: " << result.coord[1] << std::endl; 
+std::cout << "Coordenada z: " << result.coord[2] << std::endl; 
 //std::cout<<result; //esta linha não vai funcionar pois o objeto result não é um tipo simples
 
 	return 0;
