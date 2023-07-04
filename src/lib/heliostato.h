fdfddee434faa9f121e44ecae10c039fb6dc9589
Rafael Nardi <nardi.rafael@gmail.com>
2023-07-04 10:23:38 -0300

    construindo teste da função pick_point_inside_mirror_region. Função roda sem nans.

--- src/lib/heliostato.h
+++ src/lib/heliostato.h
@@ -5,7 +5,7 @@
 #include "transm_functions.h"
 
 class Heliostato {
-	private:
+	public:
 		//posição da base do heliostato e altura do eixo vertical são determinadas no construtor
 	
 		float vert_axis_height;
@@ -23,7 +23,6 @@ class Heliostato {
 		float delta_azim;
 		float delta_zenit;
 
-	public:
 	  Heliostato();
 	  
 	  /*alterações para teste*****************************************************/
@@ -33,7 +32,7 @@ class Heliostato {
 	  	/*alterações para testes**************************************************/
 		Heliostato(float x, float y, float z, float vert_axis_height, float mirror_height, float mirror_width);
 		Heliostato(vetor_3d base_pos, float vert_axis_height, float mirror_height, float mirror_width);
-		void set_normal(vetor_3d sun_pos, vetor_3d focus_pos);
+		int set_normal(vetor_3d sun_pos, vetor_3d focus_pos);
 		void set_point_inside_mirror_region(float eta_par_unit, float xi_par_unit);
 
 		//os parâmetros eta_par_unit e xi_par_unit devem ser fornecidos com valores entre 0 e 1
--- src/monte_carlo_tests/main.cpp
+++ src/monte_carlo_tests/main.cpp
@@ -1,54 +1,53 @@
 #include <iostream>
-#include "heliostato.h"
-#include "bare_functions.h"
+#include "../lib/heliostato.h"
+#include "../lib/bare_functions.h"
 /* #include "./heliostato.cpp" */
 
-int
+	int
 main ()
 {
-//Heliostato::Heliostato()
-  Heliostato result1;
-  std::cout << "Coordenada x: " << result1.base_pos.coord[0] << std::endl;
-  std::cout << "Coordenada y: " << result1.base_pos.coord[1] << std::endl;
-  std::cout << "Coordenada z: " << result1.base_pos.coord[2] << std::endl;
+	//Heliostato::Heliostato()
+	Heliostato result1;
+	std::cout << "Coordenada x: " << result1.base_pos.coord[0] << std::endl;
+	std::cout << "Coordenada y: " << result1.base_pos.coord[1] << std::endl;
+	std::cout << "Coordenada z: " << result1.base_pos.coord[2] << std::endl;
 
-  std::
-    cout <<
-    "******************************************************************************" << std::endl;;
-    std::cout<<"set_normal"<< std::endl;
-    
-    vetor_3d sun_pos(01,02,40);
-    vetor_3d focus_pos(12,43,5);
-    
-  Heliostato h1;
-  
-  h1.set_normal(sun_pos, focus_pos);
-    
-  std::
-    cout <<
-    "******************************************************************************" << std::endl;;
-    
-  
-  vetor_3d base_pos (20, 20, 0);
-  float vert_axis_height = 1.5;
-  float mirror_height = 2.0;
-  float mirror_width = 2.0;
+	std::cout << "******************************************************************************" << std::endl;;
+	std::cout<<"set_normal"<< std::endl;
 
-  Heliostato h (base_pos, vert_axis_height, mirror_height, mirror_width);
+	vetor_3d sun_pos(01,02,40);
+	vetor_3d focus_pos(12,43,5);
 
-  vetor_3d result (0, 0, 0);
+	Heliostato h1;
 
-  h.set_point_inside_mirror_region (0.2, 0.5);
-std::
-    cout <<
-    "******************************************************************************" << std::endl;;
-std::cout<<"pick_point_inside_mirror_region:"<< std::endl;
-  result = h.pick_point_inside_mirror_region (0.1, 0.3, result);
-  std::cout << "Coordenada x: " << result.coord[0] << std::endl;
-  std::cout << "Coordenada y: " << result.coord[1] << std::endl;
-  std::cout << "Coordenada z: " << result.coord[2] << std::endl;
+	h1.set_normal(sun_pos, focus_pos);
+
+	std::cout << "coordenada x da normal:" << h1.normal.coord[0] << std::endl;;
+	std::cout << "coordenada y da normal:" << h1.normal.coord[1] << std::endl;;
+	std::cout << "coordenada z da normal:" << h1.normal.coord[2] << std::endl;;
+
+	std:: cout << "******************************************************************************" << std::endl;;
 
 
+	vetor_3d base_pos (20, 20, 0);
+	float vert_axis_height = 1.5;
+	float mirror_height = 2.0;
+	float mirror_width = 2.0;
 
-  return 0;
+	Heliostato h(base_pos, vert_axis_height, mirror_height, mirror_width);
+	h.set_normal(sun_pos, focus_pos);
+
+	vetor_3d result(1,0.2,30);
+
+	/* h.set_point_inside_mirror_region (0.2, 0.5); */
+	std::cout << "******************************************************************************" << std::endl;
+	std::cout<<"pick_point_inside_mirror_region:"<< std::endl;
+	result = h.pick_point_inside_mirror_region (0.1, 0.3, result);
+	std::cout << "Coordenada x: " << result.coord[0] << std::endl;
+	std::cout << "Coordenada y: " << result.coord[1] << std::endl;
+	std::cout << "Coordenada z: " << result.coord[2] << std::endl;
+
+
+
+	return 0;
 }
